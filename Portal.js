45ccd17e0cd26fd375d93f3d861c5efa12842f22
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2022-12-13 17:01:08 +0530

    Fixed indentation in Portal.js

--- Portal.js
+++ Portal.js
@@ -3,37 +3,37 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 
-  const express = require('express')
-  const app = express()
-  const expressLayouts = require('express-ejs-layouts')
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
 
 
 
 
-  const bodyParser = require('body-parser')
-  
-  const indexRouter = require('./routes/index')
-  const authorRouter = require('./routes/authors')
-  
+const bodyParser = require('body-parser')
 
-  app.set('view engine', 'ejs')
-  app.set('views', __dirname + '/views')
-  app.set('layout', 'layouts/layout')
-  app.use(expressLayouts)
-  app.use(express.static('public'))
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
-  app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
 
-  
-  const mongoose = require('mongoose')
-  mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true })
-  const db = mongoose.connection
-  db.on('error', error => console.error(error))
-  db.once('open', () => console.log('Connected to Mongoose'))
-  
-  app.use('/', indexRouter)
-  app.use('/authors', authorRouter)
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
 
-  
-  app.listen(process.env.PORT || 3000)
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
+
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {
+useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
+
+app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+
+
+app.listen(process.env.PORT || 3000)
