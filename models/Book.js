f33111372313fcb4af68d8e4b7f1bfe2179c7bec
Royce Barker <royce_barker@msn.com>
2022-12-26 12:41:05 -0800

    Uploading imgs to the books

--- models/Book.js
+++ models/Book.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose')
-const path = require('path')
 
-const coverImageBasePath = 'uploads/bookCovers'
 
 const bookSchema = new mongoose.Schema({
   title: {
@@ -24,7 +22,11 @@ const bookSchema = new mongoose.Schema({
     required: true,
     default: Date.now
   },
-  coverImageName: {
+  coverImage: {
+    type: String,
+    required: true
+  },
+  coverImageType:{
     type: String,
     required: true
   },
@@ -36,10 +38,9 @@ const bookSchema = new mongoose.Schema({
 })
 
 bookSchema.virtual('coverImagePath').get(function() {
-  if (this.coverImageName != null) {
-    return path.join('/', coverImageBasePath, this.coverImageName)
+  if (this.coverImage != null && this.coverImageType != null) {
+    return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString('base64')}`
   }
 })
 
 module.exports = mongoose.model('Book', bookSchema)
-module.exports.coverImageBasePath = coverImageBasePath
