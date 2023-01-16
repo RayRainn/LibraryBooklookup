e3c1a5d157ea31f7816ab3a4d913bd94bd846088
Royce Barker <royce_barker@msn.com>
2023-01-16 17:20:22 +0000

    All errors when deleting on books solved thru redirect

---
+++ models/users.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+})
+
+
+module.exports = mongoose.model('users', userSchema)
